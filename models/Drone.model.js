068754b000da4a09911e67491d7863e91d2806a8
Gerard Guasch <haldawe@hotmail.com>
2023-01-11 12:43:56 +0100

    iteration 1 done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,15 @@
 // Iteration #1
+
+const { default: mongoose } = require('mongoose');
+const moongose = require('mongoose');
+const Schema = moongose.Schema;
+
+const droneSchema = new Schema ({
+        name: {type: String},
+        propellers: {type: Number},
+        maxSpeed: {type: String},
+});
+
+const Drone = mongoose.model('Drone', droneSchema);
+
+module.exports = Drone;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,27 @@
 // Iteration #1
+
+const mongoose = require("mongoose");
+const Drone = require("../models/Drone.model");
+const MONGODB_URI = "mongodb://localhost:27017/dear-santa";
+mongoose.set('strictQuery', true);
+
+const drones = [
+    { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+    { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+    { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+  ];
+
+  const connectBD = async () => {
+    try {
+      await mongoose.connect(MONGODB_URI);
+      console.log("Seed DB connected");
+      await Drone.deleteMany({});
+      const seedDB = await Drone.insertMany(drones);
+      console.log(`Seed created, ${seedDB.length} drones created`);
+      await mongoose.connection.close();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  
+  connectBD();
